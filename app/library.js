b6ac5924d148c1d99a5147704310db8815b96742
Menai Ala Eddine <ed.sheeran.eng@gmail.com>
2020-06-29 15:27:21 -0700

    refactor : convert to ES6

--- app/library.js
+++ app/library.js
@@ -1,33 +1,22 @@
 'use strict';
 
-module.exports = {
-
- /* Find the average of three integers */
- computeAverage: (num1, num2, num3) => {
-   let average;
-   // your logic goes here
-   return average;
- },
- 
- /* Find the factorial of an integer */
- computeFactorial: (num) => {
-   let factorial;
-   // your logic goes here
-   return factorial;
- },
-
- /* Convert a given Celcius temperature to Fahrenheit */
- convertTempCtoF: (cTemp) => {
-   let fTemp;
-   // your logic goes here
-   return fTemp;
- },
-
- /* Convert a given Fahrenheit temperature to Celcius */
- convertTempFtoC: (fTemp) => {
-   let cTemp;
-   // your logic goes here
-   return cTemp;
- }
-
+export function computeAverage(num1, num2, num3) {
+    let average;
+    // your logic goes here
+    return average;
+}
+export function computeFactorial(num) {
+    let factorial;
+    // your logic goes here
+    return factorial;
+}
+export function convertTempCtoF(cTemp) {
+    let fTemp;
+    // your logic goes here
+    return fTemp;
+}
+export function convertTempFtoC(fTemp) {
+    let cTemp;
+    // your logic goes here
+    return cTemp;
 }
